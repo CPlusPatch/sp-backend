1239ad696dfa2ae48c20c6132eb8212c6cdcf08d
Jesse Wierzbinski <contact@cpluspatch.com>
2024-09-03 20:47:12 +0200

    refactor: :recycle: Remove extra JSON check

--- types/schemas.ts
+++ types/schemas.ts
@@ -1,30 +1,28 @@
 import { z } from "zod";
 import type { Json } from "./responses";
 
+export const zJson = z
+    .unknown()
+    .refine(
+        (value) => {
+            try {
+                JSON.parse(JSON.stringify(value) as string);
+                return true;
+            } catch {
+                return false;
+            }
+        },
+        { message: "Invalid JSON" },
+    )
+    .transform((value) => value as Json);
+
 export const DataRowSchema = z.object({
     tags: z.array(z.string()).default([]),
     title: z.string().min(1).nullable().optional(),
     image: z.string().url().nullable().optional(),
     links: z.array(z.string().url()).default([]),
     // Any JSON value
-    data: z
-        .unknown()
-        .refine(
-            (value) => {
-                try {
-                    JSON.parse(JSON.stringify(value) as string);
-                    return true;
-                } catch {
-                    return false;
-                }
-            },
-            { message: "Invalid JSON" },
-        )
-        .transform(
-            (value) => JSON.parse(JSON.stringify(value) as string) as Json,
-        )
-        .nullable()
-        .optional(),
+    data: zJson.nullable().optional(),
     content: z.string().nullable().optional(),
 });
 
